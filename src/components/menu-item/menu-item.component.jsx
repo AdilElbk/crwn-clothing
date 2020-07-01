57f986fb3f63171fccd7a5d218ad5b1b28596e09
EL BOURKI Adil <adil.elbourki@soprabanking.com>
2020-07-01 13:45:55 +0100

    files added

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./menu-item.styles.scss";
+const MenuItem = ({ title }) => (
+  <div className="menu-item">
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./homepage.styles.scss";
+import MenuItem from "../components/menu-item/menu-item.component";
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <MenuItem title />
+      <MenuItem title />
+      <MenuItem title />
+      <MenuItem title />
+      <MenuItem title />
+    </div>
+  </div>
+);
+
+export default HomePage;
